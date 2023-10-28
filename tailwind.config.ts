974647eec608e9a2d44f592bd5bca83e50089ba0
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2023-10-28 14:59:22 +0400

    Minor tailwind config cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,8 @@
 import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './shared/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      screens: {
-        xs: '375px',
-        ...defaultTheme.screens,
-      },
-    },
-  },
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {},
   plugins: [],
 };
 export default config;
